6502488167fd6ee897f4e3948191169d9354a361
Igor D <57639034+igor981@users.noreply.github.com>
2022-04-11 18:39:29 +0200

    !

--- server/routes/auth.routes.ts
+++ server/routes/auth.routes.ts
@@ -3,6 +3,7 @@ import { logIn, signUp } from '../controllers/auth.controller';
 
 const router = Router();
 
+router.post('/test', (req, res) => res.send({ working: 'working' }));
 router.post('/signup', signUp);
 router.post('/login', logIn);
 
